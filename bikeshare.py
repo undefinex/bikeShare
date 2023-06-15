6db65821aeec341720fc6b1de8f69963b3e4ce1e
undefinex <udx@umich.edu>
2023-06-15 10:10:00 -0700

    Add comments on main func

--- bikeshare.py
+++ bikeshare.py
@@ -170,17 +170,21 @@ def user_stats(df):
 
 def main():
     while True:
+        """Main program loop"""
+
         city, month, day = get_filters()
 
         print("{}, {}, {}".format(city, month, day))
         df = load_data(city, month, day)
         print("Load data success")
 
+        # Display varies stats of the travel 
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
 
+        # Run again if enter 'yes' otherwise quit
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
